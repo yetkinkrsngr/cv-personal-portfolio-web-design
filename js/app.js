6369309cdb67b52a5a8545444f34a3e2fb17112b
yetkin karasungur <yetkinkrsngr@gmail.com>
2023-08-06 16:27:13 +0300

    some change

--- js/app.js
+++ js/app.js
@@ -23,3 +23,36 @@ if (window.innerWidth < 501) {
     });
   });
 }
+if (window.innerWidth > 1200) {
+  const carouselSlide = document.querySelector(".carosel-slider");
+  const carouselImages = document.querySelectorAll(".carosel-slider img");
+  const prevBtn = document.querySelector("#prevBtn");
+  const nextBtn = document.querySelector("#nextBtn");
+
+  // Set the initial index of the image to be displayed
+  let currentIndex = 0;
+
+  // Function to display the next image in the carousel
+  function showNextImage() {
+    currentIndex = (currentIndex + 1) % carouselImages.length;
+    updateSlidePosition();
+  }
+
+  // Function to display the previous image in the carousel
+  function showPreviousImage() {
+    currentIndex =
+      (currentIndex - 1 + carouselImages.length) % carouselImages.length;
+    updateSlidePosition();
+  }
+
+  // Function to update the position of the carousel slide
+  function updateSlidePosition() {
+    const offset = -currentIndex * carouselImages[0].clientWidth;
+    carouselSlide.style.transition = "transform 0.5s ease-in-out";
+    carouselSlide.style.transform = `translateX(${offset}px)`;
+  }
+
+  // Set event listeners for the next and previous buttons
+  nextBtn.addEventListener("click", showNextImage);
+  prevBtn.addEventListener("click", showPreviousImage);
+}
